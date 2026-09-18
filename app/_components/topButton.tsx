f223d00d745a498499bb83073a118f7fbc030cc2
Amit doliya <amitdoliya@gmail.com>
2026-09-18 16:54:13 +0530

    resloved push button error

--- app/_components/topButton.tsx
+++ app/_components/topButton.tsx
@@ -1,25 +1,18 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { ArrowUp } from "lucide-react";
 
 export default function TopButton() {
-  const router = useRouter();
-
-  const goToHome = () => {
-    router.push("/");
-  };
-
   return (
-    <button
-      type="button"
-      onClick={goToHome}
+    <Link
+      href="/"
       aria-label="Go to home page"
       className="fixed bottom-5 right-5 z-50
       bg-blue-600 text-white p-3 rounded-full
       shadow-lg hover:bg-blue-700 transition"
     >
       <ArrowUp size={24} />
-    </button>
+    </Link>
   );
 }
